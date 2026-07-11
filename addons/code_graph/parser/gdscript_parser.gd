8fa4cd3b818df9ea8f01c07fe770bb4fcb047934
Quokt <bug87@hotmail.fr>
2026-07-11 21:49:40 +0200

    update parser skeleton | add example codze

--- addons/code_graph/parser/gdscript_parser.gd
+++ addons/code_graph/parser/gdscript_parser.gd
@@ -1,12 +1,21 @@
 extends RefCounted
 
 class ScriptContent extends RefCounted:
+	var classname: String = ""
+	var parent_class: String = ""
 	var properties: Array = []
-	var export_properties: Array = []
-	var references: Array = []
 	var signals: Array = []
 	var methods: Array = []
 
+
 func parse_file(file_path: String) -> ScriptContent:
 	var script_content := ScriptContent.new()
+	var script : Script = ResourceLoader.load(file_path, "Script", ResourceLoader.CACHE_MODE_IGNORE)
+	
+	script_content.classname = script.get_global_name()
+	script_content.parent_class = script.get_base_script().get_global_name()
+	script_content.properties = script.get_script_property_list()
+	script_content.signals = script.get_script_signal_list()
+	script_content.methods = script.get_script_method_list()
+	
 	return script_content
---
+++ examples/my_node.gd
@@ -0,0 +1,11 @@
+class_name MyNode extends Node
+
+enum MY_ENUM {FOO, BAR}
+
+var my_property: String = ""
+
+@export var my_export_property: String = ""
+
+
+func my_method(arg: int) -> void:
+	return
